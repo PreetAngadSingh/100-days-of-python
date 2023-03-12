0c12af024e21e6c39bcbb486b98e28681b6939fd
PreetAngadSingh <preetangad107@gmail.com>
2023-03-12 01:37:54 -0500

    typecasting

---
+++ day7/typecasting.py
@@ -0,0 +1,24 @@
+# concatination of string in python
+a = "1"
+b = "2"
+print(a+b)
+
+# addition of integers in python
+a = 1
+b = 2
+print(a+b)
+
+# addition of string typecasted as an integer
+a = "1"
+b = "2"
+print( int(a) + int(b) )
+
+# explicit typecasting
+string_num = "15"
+num = 5
+print("The sum of string and a num is ", int(string_num) + num)
+
+# implicit typecasting - here the python interpreter converts the datatype to float
+c = 1.9
+d = 8
+print(c+d)
