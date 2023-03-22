69aab804a7bab57a2b0a13f7f81a1330e92f039f
PreetAngadSingh <preetangad107@gmail.com>
2023-03-21 22:29:32 -0400

    string basics

---
+++ day9/string.py
@@ -0,0 +1,20 @@
+name = "Preet"
+friend = "Prabh"
+anotherFriend = "Piyush"
+anotherFriend2 = "Prashant"
+
+print("Hello, " + name)
+
+# multiline string
+multiline = '''
+Hi
+this is a multiline
+text'''
+
+# this will print the second character of name
+print(name[1])
+
+# looping through a string
+print("Let's use a for loop")
+for character in name:
+    print(character)
