71eafce05e6da0f594322b3a3f3c30c947340b56
PreetAngadSingh <preetangad107@gmail.com>
2023-03-25 17:26:49 -0400

    completed day 10

--- day10/str_operations.py
+++ day10/str_operations.py
@@ -3,7 +3,7 @@ fruit = "Mango"
 # get length of the string 
 fruitLength = len(fruit)
 print(fruit,"is a", fruitLength, "letter word")
-# get first 4 characters of the string 
+# get first 4 characters of the string - means including 0 but not 4
 print(fruit[0:4])
 # this is same as fruit[0:4]
 print(fruit[:4])
@@ -11,4 +11,13 @@ print(fruit[:4])
 print(fruit[:])
 # this slices from length 0 to len-3
 print(fruit[0:len(fruit)-3])
+# this is similar to above program
 print(fruit[0:-3])
+# this will print nothing as this means [4:2]
+print(fruit[-1:-3])
+# this is same as [2:4]
+print(fruit[-3:-1])
+
+# Quick quiz 
+nm = "Preet"
+print(nm[-4:-2])
