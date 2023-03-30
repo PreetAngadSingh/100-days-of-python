bb333556ba954c3cf8573a6f282b51ac568aba6b
PreetAngadSingh <preetangad107@gmail.com>
2023-03-29 23:59:51 -0400

    user inputs

---
+++ day 8/input.py
@@ -0,0 +1,12 @@
+# This message will be displayed when the program will ask for input
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+# this will concatinate a string
+print(x+y)
+
+# this will add the inputs - as it was typecasted
+print(int(x) + int(y))
