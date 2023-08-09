90ac2e81951f96cf5ad667aec7e20c0e3d9423af
PreetAngadSingh <preetangad107@gmail.com>
2023-08-09 01:15:07 -0400

    completed for loops

--- day 15/for-loop.py
+++ day 15/for-loop.py
@@ -3,3 +3,18 @@ for i in name:
     print(i)
     if(i=='b'):
         print('This is a special b')
+
+colors = ["Red","Green","Blue","Yellow"]
+# iterating through the colors list
+for color in colors:
+    print(color)
+    #iterating through each character of the color in colors
+    for char in color:
+        print(char)
+
+# printing 20 numbers in a row 
+for k in range(20):
+    print(k+1)
+
+for k in range(5,25):
+    print(k)
