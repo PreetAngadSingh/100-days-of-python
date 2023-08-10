fd1dfc5ce91731b37d0a6b43cc1c06c6dbbee80e
PreetAngadSingh <preetangad107@gmail.com>
2023-08-09 23:59:11 -0400

    while-loop in python

---
+++ day 16/while-loop.py
@@ -0,0 +1,21 @@
+# for loop
+print("While loop")
+for i in range(3):
+    print(i)
+
+# same code with while loop
+print("While loop")
+i=0
+while(i<3):
+    print(i)
+    i=i+1
+
+print("Done with the loops")
+
+# while loop with else
+count=5
+while(count>0):
+    print(count)
+    count = count-1
+else:
+    print("Done with while, inside else")
